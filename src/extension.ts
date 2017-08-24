15739943b6b330888ca809d9b4922ee302faa883
Max Kimlyk <u131095@edu.miet.ru>
2017-08-24 17:06:16 +0300

    Improves regexps

--- src/extension.ts
+++ src/extension.ts
@@ -13,17 +13,28 @@ function repeat(line: string, amount: number) : string {
 function correctCommandLine(line: string) : string {
     var result = "";
     var words = line.split(" ");
-    
+
+    // Remove all empty words
+    words = words.filter(
+        (value: string, index:number, array: string[]) => {
+            return value != "";
+        }
+    )
+
     var expectingParameters = false;
 
     var allignings = [8, 32, 64, 80, 96, 108, 120, 132];
     var commandNumber = 0;
 
     for (var i = 0; i < words.length; i++) {
+
         var word = words[i];
 
+        if (word == "")
+            continue;
+
         // Label at the string begining
-        if (i == 0 && word.match("^\w+:$")) {
+        if (i == 0 && word.match("^\\w+:$")) {
             result += word;
             if (word.length > 7)
                 result += "\n";     // TROUBLE WITH IT - INCORRECT ACTUAL LENGTH OF STRING
@@ -32,7 +43,7 @@ function correctCommandLine(line: string) : string {
             }
             continue;
         }
-            
+
         // Command
         if (!expectingParameters) {
 
@@ -42,20 +53,18 @@ function correctCommandLine(line: string) : string {
             commandNumber++;
 
             result += word;
-            if (word.length > 7)
-                result += " ";
-            else
-                result += repeat(" ", 8 - word.length);
+            result += " ";
+
+            expectingParameters = true;
         // Parameters
         } else {
-            
-            if (!word.match("^\w+,$") && words[i+1] != ",") {
+
+            if (!word.match("^[\\w\\+\\*\\(\\)]*,$") && words[i+1] != ",") {
                 expectingParameters = false;
             }
 
             result += word;
-            if (words[i+1] != ',')
-                result += " ";
+            result += " ";
         }
 
     }
@@ -74,30 +83,37 @@ export function activate(context: vscode.ExtensionContext) {
             for (var i = 0; i < document.lineCount; i++) {
                 var currentLine = document.lineAt(i);
 
-                // Only comment in string
-                if (currentLine.text.match("^ *;.*$")) {  
-                    continue;
-                }
-
                 // Empty line
                 if (currentLine.isEmptyOrWhitespace) {
                     if (prevLineEmpty)
                         edits.push(vscode.TextEdit.delete(currentLine.rangeIncludingLineBreak));
                     prevLineEmpty = true;
+                    continue;
                 } else {
                     prevLineEmpty = false;
+                }
+
+                // Only comment in string
+                if (currentLine.text.match("^\\s*\\/?[;*].*$")) {
+                    continue;
+                }
+
+                // Directive string
+                if (currentLine.text.match("^\\s*\\.")) {
                     continue;
                 }
 
                 // Label only
-                if (currentLine.text.match("^ *\\w+: *$")) {
+                if (currentLine.text.match("^\\s*\\w+:\\s*$")) {
                     edits.push(vscode.TextEdit.replace(currentLine.range, currentLine.text.trim()));
                     continue;
                 }
-                
+
                 // Command string
-                // label: com1 r1, r2   com2 r3, r5, r6  com3 
-                var newString = correctCommandLine(currentLine.text);
+                // label: com1 r1, r2   com2 r3, r5, r6  com3
+                var str = currentLine.text;
+                var newString = correctCommandLine(str);
+                edits.push(vscode.TextEdit.replace(currentLine.range, newString));
             }
 
             return edits;
